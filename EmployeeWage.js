ba0663280ef8e22d003bbb3a047e7632963186b5
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-17 17:55:24 +0530

    [Srushti]Add.Calculate employee wages for a month till condition of max working hour or working days is reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,16 +4,25 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const WORKING_DAYS_PER_MONTH = 20;
+const MAX_WORKING_HRS_PER_MONTH = 100;
 
 let empHrs = 0;
+let totalWorkingDays = 0;
 
-for(i = 0; i < WORKING_DAYS_PER_MONTH; i++)
+while(empHrs < MAX_WORKING_HRS_PER_MONTH && totalWorkingDays < WORKING_DAYS_PER_MONTH)
 {
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = empHrs + GetWorkingHrs(empCheck);
+    let dailyWorkingHour = GetWorkingHrs(empCheck);
+    if((dailyWorkingHour + empHrs) > MAX_WORKING_HRS_PER_MONTH)
+    {
+        break;
+    }
+    empHrs = empHrs + dailyWorkingHour;
+    totalWorkingDays++;
 }
+
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total working hours : "+empHrs+"\tDaily Employee Wage : "+empWage);
+console.log("Total Working Days : "+totalWorkingDays+"\nTotal working hours : "+empHrs+"\nDaily Employee Wage : "+empWage);
 
 function GetWorkingHrs(empCheck)
 {
