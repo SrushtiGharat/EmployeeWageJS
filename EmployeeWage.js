d44587883d02bb8acfff3b18e7ad30888506791d
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-17 17:25:26 +0530

    [Srushti]Refactor.Create function to get working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,19 +5,24 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
 let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = 0;
 
-switch(empCheck)
-{
-    case IS_PRESENT_FULL_TIME:
-       empHrs = FULL_TIME_HOURS;
-       break;
-    case IS_PRESENT_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-        break;
-}
+let empHrs = GetWorkingHrs(empCheck);
+
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Daily Employee Wage : "+empWage);
+
+function GetWorkingHrs(empCheck)
+{
+    switch(empCheck)
+    {
+    case IS_PRESENT_FULL_TIME:
+       return FULL_TIME_HOURS;
+       break;
+    case IS_PRESENT_PART_TIME:
+        return PART_TIME_HOURS;
+        break;
+    default:
+        return 0;
+        break;
+    }
+}
